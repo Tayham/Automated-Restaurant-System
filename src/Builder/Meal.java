031ef1bc8859dbaa65e18dc90df938426582746e
Taylor <tayham1996@gmail.com>
2017-12-11 00:24:45 -0500

    Removed Comments

--- src/Builder/Meal.java
+++ src/Builder/Meal.java
@@ -1,7 +1,5 @@
 package Builder;
 
-import Aggregations.MenuItem;
-
 public class Meal {
 	private int mealId;
 	private String mealName;
@@ -18,41 +16,17 @@ public class Meal {
 		this.mealId = mealId;
 	}
 
-// --Commented out by Inspection START (12/7/2017 3:34 PM):
-//	public String getMealName() {
-//		return mealName;
-//	}
-// --Commented out by Inspection STOP (12/7/2017 3:34 PM)
-
 	public void setMealName(String mealName) {
 		this.mealName = mealName;
 	}
-
-// --Commented out by Inspection START (12/7/2017 3:34 PM):
-//	public String getEntree() {
-//		return entree;
-//	}
-// --Commented out by Inspection STOP (12/7/2017 3:34 PM)
 
 	public void setEntree(String entree) {
 		this.entree = entree;
 	}
 
-// --Commented out by Inspection START (12/7/2017 3:34 PM):
-//	public String getSide() {
-//		return side;
-//	}
-// --Commented out by Inspection STOP (12/7/2017 3:34 PM)
-
 	public void setSide(String side) {
 		this.side = side;
 	}
-
-// --Commented out by Inspection START (12/7/2017 3:34 PM):
-//	public String getDrink() {
-//		return drink;
-//	}
-// --Commented out by Inspection STOP (12/7/2017 3:34 PM)
 
 	public void setDrink(String drink) {
 		this.drink = drink;
@@ -67,9 +41,7 @@ public class Meal {
 	}
 
 	public String toString() {
-		//return "Meal #" + mealId + ":\n\t" + entree.toString() + "\t" + side.toString() + "\t" + drink.toString() +"\n Meal Discount Price: $" + getCost();
 		return (mealName + ":\n\t" + entree + "\n\t" + side + "\n\t" + drink + "\n Meal Cost: $" + cost + "\n");
-//		return "test";
 	}
 
 }
